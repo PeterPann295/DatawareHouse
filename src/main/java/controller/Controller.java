4f7c2ff2f4867b33eeff9a473a2c0cead48d4eeb
thoaihuu252 <140722467+thoaihuu252@users.noreply.github.com>
2024-12-04 03:17:33 +0700

    add stage + update controller
    - Stage Transform data
    - Stage Load data to warehouse
    - Controller transform + load data

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,7 +8,9 @@ import util.SendMail;
 import util.URLChecker;
 
 import java.io.FileWriter;
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -81,7 +83,73 @@ public class Controller {
             // 14.4 Gửi Mail đến Author
             SendMail.sendMail(mail, subject, message);
         }
+    }
+    public void transfromData(Connection con, LogFile logFile){
+        PhonePriceDao phonePriceDao = new PhonePriceDao();
+        try (CallableStatement callableStatement = con.prepareCall("{CALL TransformData()}")) {
+            // Thực hiện stored procedure
+            callableStatement.execute();
+            //11.
+            System.out.println("transform success!");
+            //(12. Cập nhật status của config thành TRANSFORMED
+            phonePriceDao.updateStatus(con, logFile.getId(), "TRANSFORMED");
+            //13. Thêm thông tin đã transform data vào log
+            phonePriceDao.insertLog(con, logFile.getId(), "", "Đã transform data");
+
+            //14. cập nhật trạng thái xử li file_log là đang xu ly = false
+            phonePriceDao.updateIsProcessing(con, logFile.getId(), false);
+        } catch (SQLException e) {
+            //11.1 xxet trang thai file log là fail
+            phonePriceDao.updateStatus(con, logFile.getId(), "FAIlED");
+
+            //11.2 Ghi log tranform thất bại
+            phonePriceDao.insertFileLog(con, logFile.getId(), "FAIL", "Tranform data fail");
+            //11.3cập nhật trạng thái xử li file_log là đang xu ly = false
+            phonePriceDao.updateIsProcessing(con, logFile.getId(), false);
+            // 11.4 Gửi Mail đến Author
+            String mail = logFile.getConfigFile().getEmail();
+            DateTimeFormatter dt = DateTimeFormatter.ofPattern("hh:mm:ss dd/MM/yyyy");
+            LocalDateTime nowTime = LocalDateTime.now();
+            String timeNow = nowTime.format(dt);
+            String subject = "Error Date: " + timeNow;
+            String message = "Error with message: "+e.getMessage();
+            SendMail.sendMail(mail, subject, message);
 
 
+        }
+
+    }
+    public void loadToWarehouse(Connection con, LogFile logFile) {
+        PhonePriceDao phonePriceDao = new PhonePriceDao();
+        try (CallableStatement callableStatement = con.prepareCall("{CALL LoadDataToWH()}")) {
+            // Thực hiện stored procedure
+            callableStatement.execute();
+            //12
+            System.out.println("transform success!");
+            //(13. Cập nhật status của config thành WH_LOADED
+            phonePriceDao.updateStatus(con, logFile.getId(), "WH_LOADED");
+            //14. ghi log da load data to wh
+            phonePriceDao.insertLog(con, logFile.getId(), "", "Đã load data to wh");
+            //15. cập nhật trạng thái xử li file_log là đang xu ly = false
+            phonePriceDao.updateIsProcessing(con, logFile.getId(), false);
+
+        } catch (SQLException e) {
+            //12.1 xxet trang thai file log là fail
+            phonePriceDao.updateStatus(con, logFile.getId(), "FAIlED");
+
+            //12.2 Ghi log tranform thất bại
+            phonePriceDao.insertFileLog(con, logFile.getId(), "FAIL", "load to wh data fail");
+            //12.3cập nhật trạng thái xử li file_log là đang xu ly = false
+            phonePriceDao.updateIsProcessing(con, logFile.getId(), false);
+            // 12.4 Gửi Mail đến Author
+            String mail = logFile.getConfigFile().getEmail();
+            DateTimeFormatter dt = DateTimeFormatter.ofPattern("hh:mm:ss dd/MM/yyyy");
+            LocalDateTime nowTime = LocalDateTime.now();
+            String timeNow = nowTime.format(dt);
+            String subject = "Error Date: " + timeNow;
+            String message = "Error with message: "+e.getMessage();
+            SendMail.sendMail(mail, subject, message);
+
+        }
     }
 }
---
+++ src/main/java/stage/LoadToWarehouse.java
@@ -0,0 +1,78 @@
+package stage;
+
+import controller.Controller;
+import dao.LogFileDao;
+import dao.PhonePriceDao;
+import database.DBConnection;
+import entity.LogFile;
+import util.ArgumentValidator;
+
+import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class LoadToWarehouse {
+    public static void main(String[] args) throws ParseException {
+        //1. Kiem tra Input
+        if(!ArgumentValidator.validateArgs(args)){
+            // 2.1 Yeu Cau Nguoi Dung Chay Lai Jar
+            System.out.println("Tham số không hợp lệ vui lòng truyền lại");
+            return;
+        }
+        int idConfigFile = Integer.parseInt(args[0]);
+        String dateString = args[1];
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = dateFormat.parse(dateString);
+
+        System.out.println("id_config: " + idConfigFile);
+        System.out.println("date: " + date);
+
+        DBConnection db = new DBConnection();
+        PhonePriceDao dao = new PhonePriceDao();
+        Controller controller = new Controller();
+        //3.Kết nối DB Control
+        try(
+                Connection connection = db.getConnection()) {
+
+            int countProcessing = dao.getProcessingCount(connection);
+            //4. Kiem Tra Co Process Dang Duoc Thuc Thi
+            if (countProcessing > 0) {
+                int maxWait = 0;
+                //4.1 Cho Doi 3 Phut
+                while (dao.getProcessingCount(connection) != 0 && maxWait <= 3) {
+                    System.out.println("Wait...");
+                    maxWait++;
+                    Thread.sleep(60000); //60s
+                }
+            }
+            LogFile fileLog = LogFileDao.getLogFile(connection ,idConfigFile, date);
+            //5. Kiem Tra LogFile Da Duoc Tao Hay Chua
+            if (fileLog == null) {
+                //5.1 ghi file_log load data thất bại
+                dao.insertFileLog(connection, fileLog.getId(), "FAIL", "load data to wh fail");
+            }
+            // 6. Lấy status của file log
+            String status = fileLog.getStatus();
+            //7. Kiểm tra xem status có phải laf transformed không
+            if(status.equalsIgnoreCase("transformed")){
+                //8. cap nhat trang thai xu ly file log la dang xu ly
+                dao.updateIsProcessing(connection, fileLog.getId(), true);
+                //9. cap nhat trang thai của file log
+                dao.updateStatus(connection, fileLog.getId(), "WH_LOADING");
+                //10. Ghi log bat dau load to wh
+                dao.insertLog(connection, fileLog.getId(), "", "Start load to warehouse");
+                //11. Tiến hành tranform
+                controller.loadToWarehouse(connection, fileLog);
+            } else {
+                //7.1 ghi log thaast bại nếu không phải extracted
+                dao.insertFileLog(connection, fileLog.getId(), "FAIL", "load data to wh fail");
+            }
+            db.closeConnection();
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+}
